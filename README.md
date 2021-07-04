80da68a9eb33dc15fdabbd6a8e9f486388ef84af
johnson <poshenc@gmail.com>
2021-07-04 23:26:45 +0800

    Initalise Project

---
+++ README.md
@@ -0,0 +1,24 @@
+利用Node.js + Express 建立餐廳網站
+
+功能:
+使用者可以瀏覽目前的餐廳照片、名稱、分類及評分
+使用者點擊可以查看詳細餐廳資訊
+使用者可以用中英文名稱及類別搜尋餐廳
+
+環境建置:
+Visual Studio Code
+Express 4.17.1
+Node.js
+BootStrap v4
+JQuery v3.3.1
+popper.js
+restaurant.json
+express-handlebars 5.3.2
+
+安裝流程:
+1. 開啟終端機，並cd 要放專案的位置並執行: git clone 
+2. 進入專案資料夾 cd restaurant-list
+3. 安裝 npm 套件 npm install
+4. 裝 nodemon 套件 (若未安裝) npm install -g nodemon
+5. 啟動伺服器，執行 app.js 檔案 npm run dev
+6. 當終端機出現以下字樣，表示啟動完成 The Express server is running on http://localhost:3000
---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const port = 3000
+
+// express template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public'))
+
+//routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurant: restaurantList.results })
+})
+
+//search button
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter((restaurants) => {
+    return restaurants.name.toLowerCase().includes(req.query.keyword.toLowerCase())
+  })
+  res.render('index', { restaurant: restaurants, keyword: req.query.keyword })
+})
+
+//show detail page
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.filter(function (res) {
+    return res.id === Number(req.params.restaurant_id)
+  })
+  res.render('show', { restaurant: restaurant[0] })
+})
+
+app.listen(port, () => {
+  console.log(`listing on localhost:${port}`)
+})
