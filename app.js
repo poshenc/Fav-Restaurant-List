9133849edc92994244a5be1ca129ddd67ee7cb8a
johnson <poshenc@gmail.com>
2021-07-11 18:26:32 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json')
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // express template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
