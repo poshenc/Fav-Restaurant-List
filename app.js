49250577a0446e4cdbad8658807b930ec615c122
johnson <poshenc@gmail.com>
2021-07-18 17:15:33 +0800

    modiffy express routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const Restaurant = require('./models/restaurant.js')
 const bodyParser = require('body-parser')
 
 const methodOverride = require('method-override')
+const routes = require('./routes/index')
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -33,88 +34,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
-// 首頁之routing
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
-})
-
-//新增餐廳之routing
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-
-//詳細資料之routing
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(req.params.id)
-    .lean()
-    .then((restaurants) => res.render('show', { restaurants }))
-    .catch(error => console.log(error))
-})
-
-//edit資料之routing
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(req.params.id)
-    .lean()
-    .then((restaurants) => res.render('edit', { restaurants }))
-    .catch(error => console.log(error))
-})
-
-//使用者填寫新餐廳，新增資料庫資料
-app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-
-  return Restaurant.create({ name, category, image, location, phone, google_map, rating, description })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-//使用者edit餐廳，修改資料庫資料 (Update for RESTful APL)
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurants => {
-      restaurants.name = name
-      restaurants.category = category
-      restaurants.image = image
-      restaurants.location = location
-      restaurants.phone = phone
-      restaurants.google_map = google_map
-      restaurants.rating = rating
-      restaurants.description = description
-      return restaurants.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-
-//使用者delete餐廳，修改資料庫資料(Update for RESTful APL)
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurants => restaurants.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// 使用 express.Router 獨立路由器
+app.use(routes)
 
 //search function
 app.get('/search', (req, res) => {
---
+++ routes/modules/restaurants.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant.js')
+
+
+//新增餐廳之routing
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+//詳細資料之routing
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(req.params.id)
+    .lean()
+    .then((restaurants) => res.render('show', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+//edit資料之routing
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(req.params.id)
+    .lean()
+    .then((restaurants) => res.render('edit', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+//使用者填寫新餐廳，新增資料庫資料
+router.post('', (req, res) => {
+  const name = req.body.name
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.create({ name, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//使用者edit餐廳，修改資料庫資料 (Update for RESTful APL)
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.findById(id)
+    .then(restaurants => {
+      restaurants.name = name
+      restaurants.category = category
+      restaurants.image = image
+      restaurants.location = location
+      restaurants.phone = phone
+      restaurants.google_map = google_map
+      restaurants.rating = rating
+      restaurants.description = description
+      return restaurants.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+//使用者delete餐廳，修改資料庫資料(Update for RESTful APL)
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurants => restaurants.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
+module.exports = router
